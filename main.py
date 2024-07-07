2774fcb8004b6c041f49740c10ebb6ab8bef2dc8
Tahsin Zaman <tahsin12zaman@gmail.com>
2024-07-07 22:29:07 +0600

    Added ReadMe.txt

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def process_frames(video_path, model, max_distance=50):
                 seen_parcels.add(track_id)
                 parcel_count += 1
 
-            # Draw bounding box and ID
+            # Drawing bounding box and ID
             x1, y1, x2, y2 = bbox
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
             cv2.putText(frame, f'ID: {track_id}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
